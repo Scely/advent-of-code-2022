ffb51ff8e277ee0cceb75faa69375bce129b6dad
Louis Delarue <louis.delarue@ext.adeo.com>
2022-12-04 21:21:09 +0100

    chore: update read_input

--- 03/main.py
+++ 03/main.py
@@ -18,25 +18,35 @@ def get_points_from_common_char_in_sets(*args: set) -> int:
     return char_to_priority_value(common_char)
 
 
-def part_one() -> int:
-    """https://adventofcode.com/2022/day/3"""
-    score = 0
+def read_input_file_as_compartments() -> list[set]:
     with open(INPUT_FILE) as f:
         for rucksack in f.read().splitlines():
             compartments = (
                 set(rucksack[: len(rucksack) // 2]),
                 set(rucksack[len(rucksack) // 2 :]),
             )
-            score += get_points_from_common_char_in_sets(*compartments)
+            yield compartments
+
+
+def read_input_file_as_group_of_rucksacks(nb: int = 3) -> list[set]:
+    with open(INPUT_FILE) as f:
+        while group := [set(rucksack.replace("\n", "")) for rucksack in islice(f, nb)]:
+            yield group
+
+
+def part_one() -> int:
+    """https://adventofcode.com/2022/day/3"""
+    score = 0
+    for compartments in read_input_file_as_compartments():
+        score += get_points_from_common_char_in_sets(*compartments)
     return score
 
 
 def part_two() -> int:
     """https://adventofcode.com/2022/day/3#part2"""
     score = 0
-    with open(INPUT_FILE) as f:
-        while group := [set(rucksack.replace("\n", "")) for rucksack in islice(f, 3)]:
-            score += get_points_from_common_char_in_sets(*group)
+    for group in read_input_file_as_group_of_rucksacks():
+        score += get_points_from_common_char_in_sets(*group)
     return score
 
 
