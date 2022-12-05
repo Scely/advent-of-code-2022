a5f5a5c0701d49e54ca5732487120fb4df030567
Louis Delarue <louis.delarue@ext.adeo.com>
2022-12-05 07:43:06 +0100

    wip

--- 05/main.py
+++ 05/main.py
@@ -5,18 +5,40 @@ import collections
 INPUT_FILE = "05/input.txt"
 
 
-def resolve(towers: dict[int, list[str]], rounds: list[dict[str, int]]) -> str:
+def _with_mover_9000(towers: dict[int, list[str]], rounds: list[dict[str, int]]) -> str:
     for round in rounds:
-        print(round)
         for _ in range(round["move"]):
             towers[round["to"]].append(towers[round["from"]].pop())
-        print(towers)
-    od = collections.OrderedDict(sorted(towers.items()))
-    return "".join([tower[-1] for tower in od.values()])
+    return "".join(
+        [
+            tower[-1]
+            for tower in collections.OrderedDict(sorted(towers.items())).values()
+        ]
+    )
 
 
-def read_input_file() -> dict:
-    regex_hanoi = r"(\W([A-Z])\W)|(\s\s\s\s)"
+def _with_mover_9001():
+    pass
+
+
+def top_crates(towers: dict[int, list[str]], rounds: list[dict[str, int]]) -> str:
+    for round in rounds:
+        tmp = []
+        for _ in range(round["move"]):
+            tmp.append(towers[round["from"]].pop())
+        tmp.reverse()
+        for i in tmp:
+            towers[round["to"]].append(i)
+    return "".join(
+        [
+            tower[-1]
+            for tower in collections.OrderedDict(sorted(towers.items())).values()
+        ]
+    )
+
+
+def read_input_file() -> tuple[dict[int, str], list]:
+    regex_towers = r"(\W([A-Z])\W)|(\s\s\s\s)"
     regex_moves = r"move\s(?P<move>\d+)\sfrom\s(?P<from>\d+)\sto\s(?P<to>\d+)"
     data = collections.defaultdict(list)
 
@@ -24,7 +46,7 @@ def read_input_file() -> dict:
         for line in f.read().splitlines():
             pass
             res = re.findall(
-                regex_hanoi,
+                regex_towers,
                 line,
             )
             if res:
@@ -35,22 +57,24 @@ def read_input_file() -> dict:
             res = re.match(regex_moves, line)
             if res:
                 data[0].append({k: int(v) for k, v in res.groupdict().items()})
-    return data
+
+    towers = data
+    rounds = towers.pop(0)
+    return towers, rounds
 
 
 def part_one() -> str:
     """https://adventofcode.com/2022/day/5"""
-    score = 0
-    towers = read_input_file()
-    rounds = towers.pop(0)
-    score = resolve(towers, rounds)
-    return score
+    towers, rounds = read_input_file()
+    top_crate = _with_mover_9000(towers, rounds)
+    return top_crate
 
 
-def part_two() -> int:
+def part_two() -> str:
     """https://adventofcode.com/2022/day/5#part2"""
-    score = 0
-    return score
+    towers, rounds = read_input_file()
+    top_crate = top_crates(towers, rounds)
+    return top_crate
 
 
 if __name__ == "__main__":
