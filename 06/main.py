1d8266374c2361d550fa54236e3a8900b16bb08d
Louis Delarue <louis.delarue@ext.adeo.com>
2022-12-06 08:08:27 +0100

    feat: day 6

---
+++ 06/main.py
@@ -0,0 +1,30 @@
+INPUT_FILE = "06/input.txt"
+
+
+def find_marker_index(buffer_size: int) -> int:
+    buffer = []
+    with open(INPUT_FILE) as f:
+        while True:
+            if not (char := f.read(1)):
+                break
+            buffer.append(char)
+            if len(buffer) > buffer_size:
+                buffer.pop(0)
+            if len(set(buffer)) == buffer_size:
+                return f.tell()
+    return -1
+
+
+def part_one() -> int:
+    """https://adventofcode.com/2022/day/6"""
+    return find_marker_index(4)
+
+
+def part_two() -> int:
+    """https://adventofcode.com/2022/day/6#part2"""
+    return find_marker_index(14)
+
+
+if __name__ == "__main__":
+    print(part_one())
+    print(part_two())
