773c13f6eba56c1a9b14f44d9086a3442fbf614f
Louis Delarue <louis.delarue@ext.adeo.com>
2022-12-07 18:24:27 +0100

    feat: Day 07

---
+++ 07/main.py
@@ -0,0 +1,163 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Union
+
+
+INPUT_FILE = "07/input.txt"
+
+
+class ShellWords(Enum):
+    CHANGE_DIRECTORY = "cd"
+    BANG = "$"
+    DIRECTORY = "dir"
+    FILE = "file"
+
+
+@dataclass
+class File:
+    name: str
+    parent: "Directory"
+    size: int = 0
+
+    def get_size(self) -> int:
+        return self.size
+
+    def __str__(self):
+        return f"- {self.name} ({ShellWords.FILE.value}, {self.size})"
+
+    def __int__(self):
+        return self.get_size()
+
+
+@dataclass
+class Directory:
+    name: str
+    parent: Union["Directory", None]
+    children: list[Union["Directory", None]] = field(default_factory=lambda: [])
+
+    @classmethod
+    def root(cls):
+        root_instance = cls("/", None)
+        cls.parent = root_instance
+        return root_instance
+
+    def get_size(self) -> int:
+        return sum(child.get_size() for child in self.children)
+
+    def create_subdirectory(self, name: str) -> None:
+        for child in self.children:
+            if isinstance(child, Directory) and child.name == name:
+                raise ValueError(f"Directory {name} already exists")
+        self.children.append(Directory(name, self))
+
+    def create_file(self, name: str, size: int) -> None:
+        self.children.append(File(name, self, size))
+
+    def __str__(self):
+        return f"- {self.name} ('{ShellWords.DIRECTORY.value}, {self.get_size()})"
+
+    def __int__(self):
+        return self.get_size()
+
+    def __radd__(self, other: int):
+        return self + other
+
+
+@dataclass
+class Filesystem:
+
+    available_space: int = 70000000
+    root: Directory = None
+
+    def get_child_directory(self, name: str) -> Directory:
+        for child in self.current_directory.children:
+            if isinstance(child, Directory) and child.name == name:
+                return child
+        raise ValueError(f"Child directory {name} not found")
+
+    def change_directory(self, path: str) -> None:
+        if path == "/":
+            if not self.root:
+                self.root = Directory.root()
+            self.current_directory = self.root
+        elif path == "..":
+            self.current_directory = self.current_directory.parent
+        else:
+            self.current_directory = self.get_child_directory(path)
+
+    def create_directory(self, name: str) -> None:
+        self.current_directory.create_subdirectory(name)
+
+    def create_file(self, name: str, size: int) -> None:
+        self.current_directory.create_file(name, size)
+
+    def get_directories(self) -> list[Directory]:
+        directories: list[Directory] = []
+
+        def recursive_search(node, directories: list[Directory]):
+            if isinstance(node, Directory):
+                directories.append(node)
+                for child in node.children:
+                    recursive_search(child, directories)
+
+        recursive_search(self.root, directories)
+        return directories
+
+    def tree(self):
+        def recursive_tree(node, level=0):
+            print(f"{'  ' * level}{str(node)}")
+            if isinstance(node, Directory):
+                for child in node.children:
+                    recursive_tree(child, level + 1)
+
+        recursive_tree(self.root)
+
+    def get_remaining_space(self) -> int:
+        return self.available_space - self.root.get_size()
+
+
+def read_input_file_as_filesystem() -> Filesystem:
+    fs = Filesystem()
+    with open(INPUT_FILE) as f:
+        for line in f.read().splitlines():
+            exp = line.split(" ")
+            if exp[0] == ShellWords.BANG.value:
+                if exp[1] == ShellWords.CHANGE_DIRECTORY.value:
+                    fs.change_directory(exp[2])
+            else:
+                if exp[0] == ShellWords.DIRECTORY.value:
+                    fs.create_directory(exp[1])
+                else:
+                    fs.create_file(exp[1], size=int(exp[0]))
+    return fs
+
+
+def part_one() -> int:
+    """https://adventofcode.com/2022/day/7"""
+    fs = read_input_file_as_filesystem()
+    # fs.tree()
+    directory_maximum_size = 100000
+    directory_sizes = [
+        directory.get_size()
+        for directory in fs.get_directories()
+        if directory.get_size() < directory_maximum_size
+    ]
+    return sum(directory_sizes)
+
+
+def part_two() -> int:
+    """https://adventofcode.com/2022/day/7#part2"""
+    fs = read_input_file_as_filesystem()
+    # fs.tree()
+    required_space = 30000000 - fs.get_remaining_space()
+    directory_sizes = [
+        directory.get_size()
+        for directory in fs.get_directories()
+        if directory.get_size() > required_space
+    ]
+    return min(directory_sizes)
+
+
+if __name__ == "__main__":
+    print(part_one())
+    print(part_two())
