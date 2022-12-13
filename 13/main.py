ed11f58ca43fa5ee27b5b1acc13639751afe1d05
Louis Delarue <louis.delarue@ext.adeo.com>
2022-12-13 21:03:58 +0100

    chore: UserList instead of dataclass

--- 13/main.py
+++ 13/main.py
@@ -1,65 +1,69 @@
 INPUT_FILE = "13/input.txt"
 
 import ast
-from dataclasses import dataclass
-from typing import Iterator
+from collections import UserList
+from typing import Iterator, Union
 
 from aoctools.math import mul
 
 
-@dataclass
-class Packet(list):
-    value: list
-
-    def __getitem__(self, item):
-        return self.value[item]
-
-    def __repr__(self) -> str:
-        return str(self.value)
-
-    def __add__(self, other: "Packet"):
-        return Packet(self.value + other.value)
+class Packet(UserList):
+    def __init__(self, iterable) -> None:
+        super().__init__(list(iterable))
 
     @staticmethod
-    def compare(item_left: list | int, item_right: list | int) -> bool:
+    def _recursive_comparison(item_left: list | int, item_right: list | int) -> bool:
         if isinstance(item_left, int) and isinstance(item_right, int):
             if item_left != item_right:
                 return item_left < item_right
+
         elif isinstance(item_left, list) and isinstance(item_right, list):
             for i in range(min(len(item_left), len(item_right))):
                 if item_left[i] != item_right[i]:
-                    return Packet.compare(item_left[i], item_right[i])
-            if len(item_left) > len(item_right):
-                return False
-        elif type(item_left) != type(item_right):
-            if isinstance(item_left, int):
-                return Packet.compare([item_left], item_right)
-            else:
-                return Packet.compare(item_left, [item_right])
+                    return Packet._recursive_comparison(item_left[i], item_right[i])
+            if len(item_left) != len(item_right):
+                return len(item_left) < len(item_right)
+
+        elif isinstance(item_left, int) and isinstance(item_right, list):
+            return Packet._recursive_comparison([item_left], item_right)
+
+        elif isinstance(item_left, list) and isinstance(item_right, int):
+            return Packet._recursive_comparison(item_left, [item_right])
+
         return True
 
-    def __lt__(self, other: "Packet"):
-        result = Packet.compare(self.value, other.value)
-        return result
+    @staticmethod
+    def compare(
+        item_left: Union["Packet", list, int], item_right: Union["Packet", list, int]
+    ) -> bool:
+        data_left = item_left.data if isinstance(item_left, Packet) else item_left
+        data_right = item_right.data if isinstance(item_right, Packet) else item_right
+        return Packet._recursive_comparison(data_left, data_right)
+
+    def __lt__(self, other: Union["Packet", list, int]) -> bool:
+        return self.compare(self.data, other)
+
+    def __gt__(self, other: Union["Packet", list, int]) -> bool:
+        return self.compare(other, self.data)
 
 
-def read_input_file_as_packets() -> Iterator[tuple[Packet]]:
-    packets_iter = read_input_file_as_packet()
-    for packets in packets_iter:
-        yield (packets, next(packets_iter))
-
-
-def read_input_file_as_packet() -> Iterator[Packet]:
+def read_input_file_as_packet() -> Iterator[list]:
     with open(INPUT_FILE) as f:
         for line in f.read().splitlines():
             if line:
                 yield Packet(ast.literal_eval(line))
 
 
+def read_input_file_as_pair_of_packets() -> Iterator[tuple[Packet]]:
+    packets_iter = read_input_file_as_packet()
+    for packets in packets_iter:
+        yield (packets, next(packets_iter))
+
+
 def part_one() -> int:
     """https://adventofcode.com/2022/day/13"""
     score = 0
-    for i, packets in enumerate(read_input_file_as_packets(), start=1):
+    for i, packets in enumerate(read_input_file_as_pair_of_packets(), start=1):
         packet_left, packet_right = packets
         if packet_left < packet_right:
             score += i
@@ -68,12 +72,8 @@ def part_one() -> int:
 
 def part_two() -> int:
     """https://adventofcode.com/2022/day/13#part2"""
-
     divider_packets = [Packet([[2]]), Packet([[6]])]
-
-    all_packets = sorted(
-        [packet for packet in read_input_file_as_packet()] + divider_packets
-    )
+    all_packets = sorted(list(read_input_file_as_packet()) + divider_packets)
     return mul(
         [all_packets.index(divider_packet) + 1 for divider_packet in divider_packets]
     )
